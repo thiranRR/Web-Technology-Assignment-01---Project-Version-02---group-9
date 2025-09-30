15f6b9df4d2e2c8b5babd600c8ee88073b6edc0b
sachintha karunanayaka <karunanayakakb@gmail.com>
2025-10-01 01:03:19 +0530

    home page
    include home page ,nav bar and footer

---
+++ frontend/src/components/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <h1>Scholar Connect</h1>
+      <div className="progress-bar"></div>
+    </div>
+  );
+};
+
+export default Preloader;
