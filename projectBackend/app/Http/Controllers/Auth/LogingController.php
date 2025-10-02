6ee2b392bce43d52d4105ff644d9ccceb49b4e62
Banula Bimsara <banu44bimsara@mail.com>
2025-10-02 16:46:08 +0530

    Loging controller

---
+++ projectBackend/app/Http/Controllers/Auth/LogingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogingController extends Controller
+{
+    // Login form
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    // Handle login
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    // Handle logout
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
+}
