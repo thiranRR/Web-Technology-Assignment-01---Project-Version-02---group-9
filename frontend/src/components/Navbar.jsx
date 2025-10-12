108a442f61bbb404d24d5a45e1398dd3e10e7a9a
Thiran Ranathunga <thiranranathunga2@gmail.com>
2025-10-12 20:51:49 +0530

    Resolved merge conflict in Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
-import LoginPage from './LoginPage';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -9,24 +7,10 @@ const Navbar = () => {
       <a href="#home">Home</a>
       <a href="#browse">Browse Researches</a>
       <a href="#contact">Contact</a>
+      {/* Use React Router Link for smoother navigation */}
       <Link to="/LoginPage">Login</Link>
     </nav>
   );
 };
 
-=======
-import React from 'react';
-
-const Navbar = () => {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#browse">Browse Researches</a>
-      <a href="#contact">Contact</a>
-      <a href="#login">Login</a>
-    </nav>
-  );
-};
-
->>>>>>> origin/frontend-latest
 export default Navbar;
