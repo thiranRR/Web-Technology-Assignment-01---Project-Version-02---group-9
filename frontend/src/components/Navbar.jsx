c0c5107ad92041b5971eddd297720f2e49b87713
navishd <navishmitha.gunawardana2@gmail.com>
2025-11-20 23:57:18 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginPage from './LoginPage';
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+
 
 const Navbar = () => {
   return (
     <nav>
-      {/* <a href="#home">Home</a> */}
+       <a href="/">Home</a>
       <a href="/research-opportunities">Browse Researches</a>
       <a href="/contact">Contact</a>
       <Link to="/LoginPage">Login</Link>
